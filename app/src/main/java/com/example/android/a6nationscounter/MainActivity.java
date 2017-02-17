f8fc59ce836ad5a2e8304b6a1568e2aa4fd32145
Patatartre <danielq@kth.se>
2017-02-17 14:58:25 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/a6nationscounter/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.a6nationscounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    int Try = 0 ;
+    int
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
